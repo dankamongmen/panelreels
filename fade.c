ec526033413cc44dcb7fb4a30a976178d55c7c46
nick black <dank@qemfd.net>
2011-08-19 03:09:47 -0500

    we can do it all without reproducing output FUCK YES

--- fade.c
+++ fade.c
@@ -6,16 +6,18 @@
 static short r,g,b;
 
 #define OUTCOLOR COLOR_YELLOW
+#define OUTCOLOR2 COLOR_BLUE
 
 static void
 output(WINDOW *w){
-	wclear(w);
-	assert(wattrset(w,A_DIM | COLOR_PAIR(1)) == OK);
-	hline(0,COLS);
-	assert(mvwprintw(w,1,1,"Hello world\n") == OK);
-	assert(wattrset(w,COLOR_PAIR(1)) == OK);
-	assert(mvwprintw(w,2,1,"Hello world\n") == OK);
-	assert(mvwprintw(w,3,1,"r/g/b: %hd %hd %hd\n",r,g,b) == OK);
+	if(w != curscr){
+		assert(wattrset(w,A_DIM | COLOR_PAIR(1)) == OK);
+		box(w,0,0);
+		assert(mvwprintw(w,1,1,"Hello world\n") == OK);
+		assert(wattrset(w,COLOR_PAIR(1)) == OK);
+		assert(mvwprintw(w,2,1,"Hello world\n") == OK);
+		assert(mvwprintw(w,3,1,"r/g/b: %hd %hd %hd\n",r,g,random()) == OK);
+	}
 	wrefresh(w);
 }
 
@@ -40,12 +42,26 @@ int main(void){
 	if(b) b = 1000;
 	output(scr);
 	while(r || g || b){
-		output(scr);
-		usleep(10000);
-		if(r) --r;
-		if(g) --g;
-		if(b) --b;
-		init_color(OUTCOLOR,r,g,b);
+		output(curscr);
+		// Don't want a period here above the screen's refresh rate, or
+		// else we get crappy partial updates.
+		usleep(30000);
+		if((r -= 10) < 0){
+			r = 0;
+		}
+		if((g -= 10) < 0){
+			g = 0;
+		}
+		if((b -= 10) < 0){
+			b = 0;
+		}
+		if(b / 10 % 2){
+			init_color(OUTCOLOR,r,g,b);
+			assert(init_pair(1,OUTCOLOR,-1) == OK);
+		}else{
+			init_color(OUTCOLOR2,r,g,b);
+			assert(init_pair(1,OUTCOLOR2,-1) == OK);
+		}
 	}
 	assert(endwin() == OK);
 	return EXIT_SUCCESS;
