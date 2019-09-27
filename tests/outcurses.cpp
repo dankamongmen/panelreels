5a4ff828ff561625929ad6eb6da0281e1ca2b1d9
nick black <dankamongmen@gmail.com>
2019-09-26 23:35:23 -0400

    skip ncurses tests without TERM

--- tests/outcurses.cpp
+++ tests/outcurses.cpp
@@ -1,14 +1,21 @@
 #include "main.h"
+#include <cstdlib>
 
 // We want to ensure the wide-character version of Ncurses has been linked in,
 // and that basic functionality works.
 TEST(Outcurses, Init){
+  if(getenv("TERM") == nullptr){
+    GTEST_SKIP();
+  }
   ASSERT_EQ(0, init_outcurses(true));
   ASSERT_EQ(0, stop_outcurses(true));
 }
 
 // Ensure that colors are defined and work.
 TEST(Outcurses, InitWithoutNcurses){
+  if(getenv("TERM") == nullptr){
+    GTEST_SKIP();
+  }
   ASSERT_EQ(0, init_outcurses(false));
   ASSERT_EQ(0, stop_outcurses(false));
 }
