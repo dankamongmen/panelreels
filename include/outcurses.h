e1f1101a901e99d0a8e11e108adac17463199e42
nick black <dankamongmen@gmail.com>
2019-09-26 21:31:33 -0400

    prefix: PowersOfTenAsTwos, fix mantissa bug for 1024 (#16)

--- include/outcurses.h
+++ include/outcurses.h
@@ -23,13 +23,21 @@ int stop_outcurses(bool stopcurses);
 int fade(WINDOW* w, unsigned ms);
 
 #define PREFIXSTRLEN 7  // Does not include a '\0' (xxx.xxU)
-#define BPREFIXSTRLEN 9  // Does not include a '\0' (xxx.xxUi), i == prefix
+#define BPREFIXSTRLEN 9  // Does not include a '\0' (xxxx.xxUi), i == prefix
 #define PREFIXFMT "%7s"
 #define BPREFIXFMT "%9s"
 
 // Takes an arbitrarily large number, and prints it into a fixed-size buffer by
-// adding the necessary SI suffix. Usually, pass a |PREFIXSTRLEN+1|-sized
-// buffer to generate up to PREFIXSTRLEN characters.
+// adding the necessary SI suffix. Usually, pass a |[B]PREFIXSTRLEN+1|-sized
+// buffer to generate up to [B]PREFIXSTRLEN characters. The characteristic can
+// occupy up through |mult-1| characters (3 for 1000, 4 for 1024). The mantissa
+// can occupy either zero or two characters.
+//
+// Floating-point is never used, because an IEEE758 double can only losslessly
+// represent integers through 2^53-1.
+//
+// 2^64-1 is 18446744073709551615, 18.45E(xa). KMGTPEZY thus suffice to handle
+// a 89-bit uintmax_t. Beyond Z(etta) and Y(otta) lie lands unspecified by SI.
 //
 // val: value to print
 // decimal: scaling. '1' if none has taken place.
@@ -42,7 +50,7 @@ int fade(WINDOW* w, unsigned ms);
 static inline const char *
 genprefix(uintmax_t val, unsigned decimal, char *buf, size_t bsize, 
 			int omitdec, unsigned mult, int uprefix){
-	const char prefixes[] = "KMGTPE"; // 10^21-1 encompasses 2^64-1
+	const char prefixes[] = "KMGTPEZY"; // 10^21-1 encompasses 2^64-1
 	unsigned consumed = 0;
 	uintmax_t dv;
 
@@ -50,7 +58,7 @@ genprefix(uintmax_t val, unsigned decimal, char *buf, size_t bsize,
 		return NULL;
 	}
 	dv = mult;
-	// FIXME verify that input < 2^64, wish we had static_assert() :/
+	// FIXME verify that input < 2^89, wish we had static_assert() :/
 	while((val / decimal) >= dv && consumed < strlen(prefixes)){
 		dv *= mult;
 		++consumed;
@@ -65,22 +73,27 @@ genprefix(uintmax_t val, unsigned decimal, char *buf, size_t bsize,
 			dv /= mult;
 		}
 		val /= decimal;
-		// Remainder is val % dv; we want a percentage as scaled integer
-		unsigned long remain = (val % dv) / (dv / 100);
+		// Remainder is val % dv, but we want a percentage as scaled integer.
+		// Ideally we would multiply by 100 and divide the result by dv, for
+		// maximum accuracy (dv might not be a multiple of 10--it is not for
+		// 1,024). That can overflow with large 64-bit values, but we can at
+		// least scale by 10 for all valid (val % dv). This yields enough
+		// accuracy to be correct through a two digit mantissa.
+		uintmax_t remain = (val % dv) * 10 / (dv / 10);
 		if(remain || omitdec == 0){
 			// FIXME we throw the % 100 on remain to avoid a
 			// format-truncation warning. remain ought always be
 			// less than 100, since integer division goes to 0.
-			snprintf(buf, bsize, "%ju.%02lu%c%c",
-					val / dv,
-					remain % 100,
-					prefixes[consumed - 1],
-					uprefix);
+			snprintf(buf, bsize, "%ju.%02ju%c%c",
+					 val / dv,
+					 remain % 100,
+					 prefixes[consumed - 1],
+					 uprefix);
 		}else{
 			snprintf(buf, bsize, "%ju%c%c", val / dv,
-						prefixes[consumed - 1], uprefix);
+					 prefixes[consumed - 1], uprefix);
 		}
-	}else{
+	}else{ // unscaled output, consumed == 0, dv == mult
 		if(val % decimal || omitdec == 0){
 			snprintf(buf, bsize, "%ju.%02ju", val / decimal, val % decimal);
 		}else{
