5190512f9bbd68ec31e4b07476f0bc97ceb94e0a
nick black <dankamongmen@gmail.com>
2019-10-18 03:27:24 -0400

    demo: create a panelreel

--- src/bin/demo.c
+++ src/bin/demo.c
@@ -1,6 +1,33 @@
 #include <stdlib.h>
 #include <outcurses.h>
+#include <panelreel.h>
 
 int main(void){
-	return EXIT_SUCCESS;
+  int ret = EXIT_FAILURE;
+
+  if(init_outcurses(true)){
+    fprintf(stderr, "Error initializing outcurses\n");
+    return EXIT_FAILURE;
+  }
+  panelreel_options popts = {
+    .infinitescroll = true,
+    .circular = true,
+  };
+  struct panelreel* pr = create_panelreel(&popts);
+  if(pr == NULL){
+    fprintf(stderr, "Error creating panelreel\n");
+    goto done;
+  }
+  if(destroy_panelreel(pr)){
+    fprintf(stderr, "Error destroying panelreel\n");
+    goto done;
+  }
+  ret = EXIT_SUCCESS;
+
+done:
+  if(stop_outcurses(true)){
+    fprintf(stderr, "Error initializing outcurses\n");
+    return EXIT_FAILURE;
+  }
+	return ret;
 }
