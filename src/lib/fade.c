c373dee47bb30a43b29cda01cc4792e9d7c0cb3e
nick black <dankamongmen@gmail.com>
2019-11-08 21:07:42 -0500

    implement fade-in, run it in demo #24

--- src/lib/fade.c
+++ src/lib/fade.c
@@ -4,16 +4,11 @@
 #include <sys/time.h>
 #include <outcurses.h>
 
-// A set of RGB color components
-typedef struct ccomps {
-  int r, g, b;
-} ccomps;
-
 // These arrays are too large to be safely placed on the stack.
 static int
-alloc_ccomps(int count, ccomps** orig, ccomps** cur){
-  *orig = malloc(count * sizeof(ccomps));
-  *cur = malloc(count * sizeof(ccomps));
+alloc_palette(int count, outcurses_rgb** orig, outcurses_rgb** cur){
+  *orig = malloc(count * sizeof(outcurses_rgb));
+  *cur = malloc(count * sizeof(outcurses_rgb));
   if(*orig == NULL || *cur == NULL){
     free(*orig);
     free(*cur);
@@ -24,12 +19,9 @@ alloc_ccomps(int count, ccomps** orig, ccomps** cur){
   return 0;
 }
 
-// count ought be COLORS to retrieve the entire palette. If maxes is non-NULL,
-// it points to a single ccomps structure, which will be filled in with the
-// maximum components found.
-static int
-retrieve_palette(int count, ccomps* palette, ccomps* maxes){
-  ccomps maxes_store;
+int retrieve_palette(int count, outcurses_rgb* palette, outcurses_rgb* maxes,
+                     bool zeroout){
+  outcurses_rgb maxes_store;
   int p;
 
   if(maxes == NULL){
@@ -49,12 +41,16 @@ retrieve_palette(int count, ccomps* palette, ccomps* maxes){
     if(palette[p].b > maxes->b){
       maxes->b = palette[p].b;
     }
+    if(zeroout){
+      if(init_extended_color(p, 0, 0, 0) != OK){
+        return -1;
+      }
+    }
   }
   return 0;
 }
 
-static int
-set_palette(int count, const ccomps* palette){
+int set_palette(int count, const outcurses_rgb* palette){
   int p;
 
   for(p = 0 ; p < count ; ++p){
@@ -71,20 +67,20 @@ set_palette(int count, const ccomps* palette){
 int fadeout(WINDOW* w, unsigned ms){
   uint64_t nanosecs_total;
   uint64_t nanosecs_step;
-  ccomps* orig;
-  ccomps maxes;
+  outcurses_rgb* orig;
+  outcurses_rgb maxes;
   int maxsteps;
-  ccomps* cur;
+  outcurses_rgb* cur;
   int ret;
 
   ret = -1;
-  if(alloc_ccomps(COLORS, &orig, &cur)){
+  if(alloc_palette(COLORS, &orig, &cur)){
     goto done;
   }
   // Retrieve current palette, and extract component maxima. There is no
   // point in doing more loop iterations than the maximum component, since
   // the smallest action we can take is subtracting 1 from the largest value.
-  if(retrieve_palette(COLORS, orig, &maxes)){
+  if(retrieve_palette(COLORS, orig, &maxes, false)){
     goto done;
   }
   maxsteps = maxes.g > maxes.r ? (maxes.b > maxes.g ? maxes.b : maxes.g) :
@@ -148,7 +144,68 @@ done:
   return ret;
 }
 
-int fadein(WINDOW* w, unsigned ms){
-  (void)w; (void)ms; // FIXME
-  return -1;
+static int
+find_palette_max(int count, const outcurses_rgb* palette){
+  int max = 0;
+  while(count--){
+    if(palette[count].r > max){
+      max = palette[count].r;
+    }
+    if(palette[count].g > max){
+      max = palette[count].g;
+    }
+    if(palette[count].b > max){
+      max = palette[count].b;
+    }
+  }
+  return max;
+}
+
+int fadein(WINDOW* w, int count, const outcurses_rgb* palette, unsigned ms){
+  int max = find_palette_max(count, palette);
+  uint64_t nanosecs_total = ms * NANOSECS_IN_MS;
+  // Number of nanoseconds in an ideal steptime
+  uint64_t nanosecs_step = nanosecs_total / max;
+  struct timespec times;
+  clock_gettime(CLOCK_MONOTONIC, &times);
+  // Start time in absolute nanoseconds
+  uint64_t startns = times.tv_sec * NANOSECS_IN_SEC + times.tv_nsec;
+  // Current time, sampled each iteration
+  uint64_t curns;
+  int ret = -1;
+  int p;
+  outcurses_rgb* cur = malloc(sizeof(*cur) * count);
+  do{
+    clock_gettime(CLOCK_MONOTONIC, &times);
+    curns = times.tv_sec * NANOSECS_IN_SEC + times.tv_nsec;
+    int iter = (curns - startns) / nanosecs_step + 1;
+    if(iter > max){
+      break;
+    }
+    for(p = 0 ; p < COLORS ; ++p){
+      cur[p].r = palette[p].r * iter / max;
+      cur[p].g = palette[p].g * iter / max;
+      cur[p].b = palette[p].b * iter / max;
+      if(init_extended_color(p, cur[p].r, cur[p].g, cur[p].b) != OK){
+        goto done;
+      }
+    }
+    wrefresh(w);
+    uint64_t nextwake = (iter + 1) * nanosecs_step + startns;
+    struct timespec sleepspec;
+    sleepspec.tv_sec = nextwake / NANOSECS_IN_SEC;
+    sleepspec.tv_nsec = nextwake % NANOSECS_IN_SEC;
+    int r;
+    // clock_nanosleep() has no love for CLOCK_MONOTONIC_RAW, at least as
+    // of Glibc 2.29 + Linux 5.3 :/.
+    r = clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &sleepspec, NULL);
+    if(r){
+      goto done;
+    }
+  }while(true);
+  ret = 0;
+
+done:
+  free(cur);
+  return ret;
 }
