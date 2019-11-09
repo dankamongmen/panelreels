5afe8c0ecfd47963ddb8d102b0361211321c35e7
nick black <dankamongmen@gmail.com>
2019-11-09 13:32:31 -0500

    fade-in unit test

--- tests/fade.cpp
+++ tests/fade.cpp
@@ -2,35 +2,54 @@
 #include <cstdlib>
 #include <iostream>
 
+void fade_setup(WINDOW* w) {
+  wborder(w, 0, 0, 0, 0, 0, 0, 0, 0);
+  wmove(w, 1, 1);
+  const auto PERLINE = 16;
+  for(int i = 0 ; i < COLORS ; i += PERLINE){
+    wmove(w, i / PERLINE + 1, 1);
+    for(int j = 0 ; j < PERLINE ; ++j){
+      if(i + j >= COLORS){
+        break;
+      }
+      wattrset(w, COLOR_PAIR(i + j));
+      wprintw(w, "*");
+      wattrset(w, A_DIM | COLOR_PAIR(i + j));
+      wprintw(w, "*");
+      wattrset(w, A_BOLD | COLOR_PAIR(i + j));
+      wprintw(w, "*");
+      if(j % PERLINE == PERLINE / 2){
+        wprintw(w, " ");
+      }
+	  }
+    wprintw(w, " (%3d)", i);
+  }
+  wrefresh(w);
+}
+
 TEST(OutcursesFade, FadeOut) {
   if(getenv("TERM") == nullptr){
 	  GTEST_SKIP();
   }
   ASSERT_NE(nullptr, init_outcurses(true));
-  wborder(stdscr, 0, 0, 0, 0, 0, 0, 0, 0);
-  wmove(stdscr, 1, 1);
-  const auto PERLINE = 16;
-  for(int i = 0 ; i < COLORS ; i += PERLINE){
-    wmove(stdscr, i / PERLINE + 1, 1);
-    for(int j = 0 ; j < PERLINE ; ++j){
-      if(i + j >= COLORS){
-        break;
-      }
-      wattrset(stdscr, COLOR_PAIR(i + j));
-      wprintw(stdscr, "*");
-      wattrset(stdscr, A_DIM | COLOR_PAIR(i + j));
-      wprintw(stdscr, "*");
-      wattrset(stdscr, A_BOLD | COLOR_PAIR(i + j));
-      wprintw(stdscr, "*");
-      if(j % PERLINE == PERLINE / 2){
-        wprintw(stdscr, " ");
-      }
-	  }
-    wprintw(stdscr, " (%3d)", i);
-  }
-  wrefresh(stdscr);
+  fade_setup(stdscr);
   struct timespec ts = {.tv_sec = 0, .tv_nsec = 500000000, };
   nanosleep(&ts, NULL);
   ASSERT_EQ(0, fadeout(stdscr, 1500));
   ASSERT_EQ(0, stop_outcurses(true));
+}
+
+TEST(OutcursesFade, FadeIn) {
+  if(getenv("TERM") == nullptr){
+	  GTEST_SKIP();
+  }
+  ASSERT_NE(nullptr, init_outcurses(true));
+  outcurses_rgb* palette = new outcurses_rgb[COLORS];
+  retrieve_palette(COLORS, palette, nullptr, true);
+  fade_setup(stdscr);
+  ASSERT_EQ(0, fadein(stdscr, COLORS, palette, 1500));
+  struct timespec ts = {.tv_sec = 0, .tv_nsec = 500000000, };
+  nanosleep(&ts, NULL);
+  ASSERT_EQ(0, stop_outcurses(true));
+  delete[] palette;
 }
